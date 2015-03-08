8e8c8b375497a04c9d93d6cead053751a19d7560
theDeviloper <t.stehle@live.de>
2015-03-08 17:07:27 +0100

    Update kontakt.blade.php

--- resources/views/facts/kontakt.blade.php
+++ resources/views/facts/kontakt.blade.php
@@ -9,47 +9,35 @@ Willkommen auf Blah...
 @stop
 
 @section('content')
-	<div class="container marketing">
-      <!-- Drei Spalten Text unter dem Karussell -->
-      <div class="row">
-		<form action="kontakt" method="post" id="contact_form" novalidate="">
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-          <div class="col-sm-6">
-          <label>Vorname*</label>
-          <input type="text" name="vorname" id="vorname" class="form-control" value="">
-          </div>
-        </form>
-	  </div><!-- /.row -->
-    </div>
+<div class="container marketing">
+<!-- Drei Spalten Text unter dem Karussell -->
+  <div class="row">
+    <form action="kontakt" method="post" id="contact_form" novalidate="">
+      <div class="col-sm-6">
+        <label>Betreff*</label>
+        <input type="text" name="betreff" id="betreff" class="form-control" value="">
+      </div>
+      <div class="col-sm-6">
+        <label>Vorname*</label>
+        <input type="text" name="vorname" id="vorname" class="form-control" value="">
+      </div>
+      <div class="col-sm-6">
+        <label>Nachname*</label>
+        <input type="text" name="nachname" id="nachname" class="form-control" value="">
+      </div>
+      <div class="col-sm-6">
+        <label>E-Mail-Adresse*</label>
+        <input type="text" name="e-mail" id="e-mail" class="form-control" value="">
+      </div>
+      <div class="col-sm-12">
+        <label>Nachricht*</label>
+        <textarea type="text" name="nachricht" id="nachricht" class="form-control"></textarea>
+      </div>
+      <div class="col-sm-12">
+        <label></label>
+        <input type="submit" name="absenden" id="absenden" class="form-control" value="Absenden">
+      </div>
+    </form>
+  </div><!-- /.row -->
+</div>
 @stop
